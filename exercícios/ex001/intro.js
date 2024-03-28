209f2440815cb989ac08f2bbf1cd79aa72c67720
João Pedro Monteiro <154384458+monteiroojp@users.noreply.github.com>
2024-03-28 20:00:49 -0300

    hello world

---
+++ exercícios/ex001/intro.js
@@ -0,0 +1,11 @@
+const number = 9
+
+if(number < 10){
+    console.log('This is a short number')
+}
+
+else{
+    console.log('This is a big Number')
+}
+
+console.log('Ebaa, hello world, primeiro projeto mega simples com essa lindeza')
