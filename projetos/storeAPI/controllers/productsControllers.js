6fbe87021e5d11a41988ae8404df17c22b8bd9eb
João Pedro Monteiro <154384458+monteiroojp@users.noreply.github.com>
2024-05-06 19:53:23 -0300

    gg

--- projetos/storeAPI/controllers/productsControllers.js
+++ projetos/storeAPI/controllers/productsControllers.js
@@ -4,9 +4,33 @@ const asyncWrapper = require('../middlewares/asyncwrapper')
 //Async Wrapper
 
 //Get all products
-const getAllProducts = asyncWrapper(async (req, res) => {
-        const product = await Product.find({})
-        res.status(200).json({product})
+const getAllProducts =  asyncWrapper(async (req, res) => {
+    const {featured, name, sort, select} = req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === 'true'? true: false
+    }
+    if(name){
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
+
+    let result = Product.find(queryObject)
+
+    if(sort){
+        const sortList = sort.split(',').join(' ') 
+        result = result.sort(sortList)
+    }
+
+    if(select){
+        const selectList = select.split(',').join(' ')
+        result = result.select(selectList)
+    }
+ 
+
+    console.log(queryObject)
+    const products = await result
+    res.status(200).json({products})
 })
 
 //Create Product
